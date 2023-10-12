ed51f9753e4806151a63b863c1c8005d477b6c9b
saratchbehera <saratchbehera88@gmail.com>
2023-10-13 00:03:34 +0530

    SOLID Principle

--- src/main/java/thelazyguy/learnjava/solidprinciple/lsp/SocialMedia.java
+++ src/main/java/thelazyguy/learnjava/solidprinciple/lsp/SocialMedia.java
@@ -2,5 +2,5 @@ package thelazyguy.learnjava.solidprinciple.lsp;
 
 public interface SocialMedia {
    public void chatWithFriend();
-   public void sendPhotosAndVideos()
+   public void sendPhotosAndVideos();
 }
